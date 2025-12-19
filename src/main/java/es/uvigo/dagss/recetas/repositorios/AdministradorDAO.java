e6bf5588f9662bc55034082b3d077c4d3677ebcd
PabloCorreaP <pablocorreapastor2001@gmail.com>
2025-12-19 10:53:16 +0100

    correcion  enpoints

--- src/main/java/es/uvigo/dagss/recetas/repositorios/AdministradorDAO.java
+++ src/main/java/es/uvigo/dagss/recetas/repositorios/AdministradorDAO.java
@@ -4,7 +4,9 @@ import es.uvigo.dagss.recetas.entidades.Administrador;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//* jspecificatioexecutor
+// findcell(Specification<cliente> pageable)
+// ver en ejemplo cliente controller ejemplo bucar todos */
 public interface AdministradorDAO extends JpaRepository<Administrador, Long> {
 
     Optional<Administrador> findByLogin(String login);
